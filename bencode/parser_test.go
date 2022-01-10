19bb6822874661ab9f931448c0a9f2c8b80bd268
archeryue <archeryue7@gmail.com>
2022-01-10 13:10:34 +0800

    MOD: parser testcase

--- bencode/parser_test.go
+++ bencode/parser_test.go
@@ -7,20 +7,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func objAssertStr(t *testing.T, expect string, o *BObject) {
+	assert.Equal(t, BSTR, o.type_)
+	str, err := o.String()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, expect, str)
+}
+
+func objAssertInt(t *testing.T, expect int, o *BObject) {
+	assert.Equal(t, BINT, o.type_)
+	val, err := o.Int()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, expect, val)
+}
+
 func TestParseString(t *testing.T) {
 	var o *BObject
 	buf := bytes.NewBufferString("3:abc")
 	o, _ = Parse(buf)
-	assert.Equal(t, BSTR, o.type_)
-	assert.Equal(t, "abc", o.val_.(string))
+	objAssertStr(t, "abc", o)
 }
 
 func TestParseInt(t *testing.T) {
 	var o *BObject
 	buf := bytes.NewBufferString("i123e")
 	o, _ = Parse(buf)
-	assert.Equal(t, BINT, o.type_)
-	assert.Equal(t, 123, o.val_.(int))
+	objAssertInt(t, 123, o)
 }
 
 func TestParseList(t *testing.T) {
@@ -29,14 +41,12 @@ func TestParseList(t *testing.T) {
 	buf := bytes.NewBufferString("li123e6:archeri789ee")
 	o, _ = Parse(buf)
 	assert.Equal(t, BLIST, o.type_)
-	list = o.val_.([]*BObject) 
+	list, err := o.List()
+	assert.Equal(t, nil, err)
 	assert.Equal(t, 3, len(list))
-	assert.Equal(t, BINT, list[0].type_)
-	assert.Equal(t, 123, list[0].val_.(int))
-	assert.Equal(t, BSTR, list[1].type_)
-	assert.Equal(t, "archer", list[1].val_.(string))
-	assert.Equal(t, BINT, list[2].type_)
-	assert.Equal(t, 789, list[2].val_.(int))
+	objAssertInt(t, 123, list[0])
+	objAssertStr(t, "archer", list[1])
+	objAssertInt(t, 789, list[2])
 }
 
 func TestParseMap(t *testing.T) {
@@ -45,12 +55,8 @@ func TestParseMap(t *testing.T) {
 	buf := bytes.NewBufferString("d4:name6:archer3:agei29ee")
 	o, _ = Parse(buf)
 	assert.Equal(t, BDICT, o.type_)
-	dict = o.val_.(map[string]*BObject)
-	var val *BObject
-	val = dict["name"]
-	assert.Equal(t, BSTR, val.type_)
-	assert.Equal(t, "archer", val.val_.(string))
-	val = dict["age"]
-	assert.Equal(t, BINT, val.type_)
-	assert.Equal(t, 29, val.val_.(int))
+	dict, err := o.Dict()
+	assert.Equal(t, nil, err)
+	objAssertStr(t, "archer", dict["name"])
+	objAssertInt(t, 29, dict["age"])
 }
