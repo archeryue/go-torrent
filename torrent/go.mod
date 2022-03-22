bd2f891f504dc0dd659d2ca986870ecd4c451600
archeryue <archeryue7@gmail.com>
2022-03-22 15:12:50 +0800

    ADD: raw torrent file parse

--- torrent/go.mod
+++ torrent/go.mod
@@ -1,3 +1,7 @@
 module github.com/archeryue/go-torrent/torrent
 
 go 1.17
+
+require github.com/archeryue/go-torrent/bencode v0.0.0-20220320082858-9628ecdf6dfa
+
+replace github.com/archeryue/go-torrent/bencode => ../bencode
--- torrent/torrent_file.go
+++ torrent/torrent_file.go
@@ -1,8 +1,23 @@
 package torrent
 
 import (
+	"fmt"
 	"io"
+
+	"github.com/archeryue/go-torrent/bencode"
 )
+
+type rawInfo struct {
+	Name string		`bencode:"name"`
+	Length int		`bencode:"length"`
+	pieceLength int	`bencode:"piece length"`
+	pieces []byte	`bencode:"pieces"`
+}
+
+type rawFile struct {
+	Announce string	`bencode:"announce"`
+	info rawInfo	`bencode:"info"`
+}
 
 type TorrentFile struct {
 	Announce string
@@ -14,6 +29,17 @@ type TorrentFile struct {
 }
 
 func ParseFile(r io.Reader) (*TorrentFile, error) {
-	return nil, nil
-	//TODO
+	raw := new(rawFile)
+	err := bencode.Unmarshal(r, raw)
+	if err != nil {
+		fmt.Println("Fail to parse torrent file")
+		return nil, err
+	}
+	ret := new(TorrentFile)
+	ret.Announce = raw.Announce
+	ret.FileName = raw.info.Name
+	ret.FileLen = raw.info.Length
+	ret.PieceLen = raw.info.pieceLength
+	//TODO: InfoSHA & PieceSHA
+	return ret, nil
 }
