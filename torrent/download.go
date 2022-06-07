0bf41d9c86d7fcc0a575a6eb1f9c12886d15d4f9
archeryue <archeryue7@gmail.com>
2022-06-07 14:52:37 +0800

    MOD: dowload logic

--- torrent/download.go
+++ torrent/download.go
@@ -10,10 +10,37 @@ type TorrentTask struct {
 	PieceSHA [][SHALEN]byte
 }
 
+type pieceTask struct {
+	index  int
+	sha    [SHALEN]byte
+	length int
+}
+
+type taskState struct {
+	index      int
+	conn       *PeerConn
+	requested  int
+	downloaded int
+	backlog    int
+	data       []byte
+}
+
+type pieceResult struct {
+	index int
+	data  []byte
+}
+
+const BLOCKSIZE = 16384
+const MAXBACKLOG = 5
+
+func downloadPiece(conn *PeerConn, task pieceTask) (pieceResult, error) {
+	return pieceResult{}, nil
+}
+
 func Download(task *TorrentTask) error {
-	//TODO: check local tmp file
-	//TODO: download piceces and check
-	//TODO: write picece bytes into local tmp file
-	//TODO: change tmp file name
+	// TODO: split pieceTasks and init task&result channel
+	// TODO: init goroutines for each peer
+	// TODO: check result channel
+	// TODO: create file & copy data
 	return nil
 }
